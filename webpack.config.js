c3ab752d04cff57c6b792a5b1898c901347bd74f
unknown <you@example.com>
2021-09-09 11:50:44 +0000

    c1009

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,8 @@ module.exports = {
   plugins: [
     new ModuleFederationPlugin({
         remotes: {
-            "productlistmfe": `productlistmfeapp@{PRODLIST_URL}`,
-            "cartmfe": `cartmfeapp@{CART_URL}`,
+            "productlistmfe": `productlistmfeapp@{prodlistdomain}`,
+            "cartmfe": `cartmfeapp@{cartdomain}`,
             //"paymentmfe": "paymentmfeapp@http://localhost:1111/remoteEntry.js"
         },
 
